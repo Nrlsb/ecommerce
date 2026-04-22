2e9160491a7c328f4ff93a0b780a75c602af0993
Nrlsb <luksbenitez01@hotmail.com>
2026-04-22 10:19:09 -0300

    Actualizacion 1.0.0694

--- src/app/catalogo/page.tsx
+++ src/app/catalogo/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { Loader2 } from 'lucide-react';
 import { supabase } from '@/lib/supabase';
 import { useCart } from '@/context/CartContext';
 import { FilterPanel } from '@/components/catalogo/FilterPanel';
