5105cea2246346de45267c72a3265295c9f34e0c
Nrlsb <luksbenitez01@hotmail.com>
2026-04-23 14:08:02 -0300

    Actualizacion 1.0.06994

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,12 +6,12 @@ export const env = {
 // Validación para asegurar que las variables están presentes
 if (!env.supabaseUrl || !env.supabaseAnonKey) {
   const errorMsg = 'Error: NEXT_PUBLIC_SUPABASE_URL o NEXT_PUBLIC_SUPABASE_ANON_KEY no están definidas.';
-  
+
   if (typeof window === 'undefined') {
     // En el servidor (build time / server side) lanzamos error
     console.error(errorMsg);
   } else {
-    // En el cliente, informamos por consola si algo falló
+    // En el  cliente, informamos por consola si algo falló
     console.error(errorMsg);
   }
 }
